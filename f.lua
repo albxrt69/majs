4bdae0a0cbd5fffa2d3aa65c93f8fe6a1ab14acb
albxrt69 <107062917+albxrt69@users.noreply.github.com>
2023-08-02 12:54:06 +0300

    Add files via upload

---
+++ f.lua
@@ -0,0 +1,12 @@
+--[[
+
+Welcome to Fluxus!
+
+Do you need help with Fluxus? Join our discord! discord.gg\GNHbGPbah2
+
+Check out Fluxus on Android! www.fluxteam.net/android
+
+Tired of the keysystem and ads? Level up by purchasing Fluxus Premium!
+Join our discord for more information OR check out www.bloxproducts.com/#fluxus
+
+--]]
